025d02e5d69c326f29e25ea8c0855e849c7c67c3
Yoonsung <jeffkim1118@outlook.com>
2021-09-23 17:35:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(name, string){
+    return name.filter(function(list){
+        return list.toLowerCase().indexOf(string.toLowerCase()) !==-1;
+    });
+}
+
+function fuzzyMatch(names, query){
+    const length = query.length;
+    return names.filter(function(el){
+       // return el.toLowerCase().indexOf(query.toLowerCase()) !==-1;
+       return el.slice(0,length).toLowerCase() === query.toLowerCase();
+    });
+}
+
+function matchName(drivers, query){
+    return drivers.filter(function(el){
+        return el.name === query;
+    })
+}
